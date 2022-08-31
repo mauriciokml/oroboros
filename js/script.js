caf21b6de5667acdcaf4343c360e7ec635eab357
Mauricio KML <mauriciu_u@hotmail.com>
2022-08-31 11:26:07 -0300

    parametros url add

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,6 +10,26 @@ const url = location.href;
 
 }
 links.forEach(ativarLink);
+
+
+// Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+}
+
+
+parametros.forEach(ativarProduto);
+
+
+
+
 
 
 const imagens = document.querySelectorAll('#galeria-mel img');
